6c5f0015b62f687d0cda8d293ce4f1a2e246e272
Mostafa Shams <70518039+MostafaShamsss@users.noreply.github.com>
2021-05-22 22:20:57 +0200

    identifier possibilities covered

--- src/Scanner.java
+++ src/Scanner.java
@@ -10,10 +10,15 @@ public class Scanner
     {
         String inputLine = new String("read x; {shs shsh shs}ss ss");
         ArrayList<Character> charArray = new ArrayList<Character>();
+        String [] reservedWords = {"if","then","else","end","repeat","until","read","write"};
         States currentState =States.START;
-        int k, j=0;
-        String[] identifierList= new String[50];
-        ArrayList<String> tokenList = new ArrayList<String>();
+        States previousState = States.START;
+        //int k, j=0;
+        ArrayList<String> identifierList= new ArrayList<String>();
+        String identifier1 = new String();
+        ArrayList<String> numberList= new ArrayList<String>();
+        String number1 = new String();
+        ArrayList<Token> tokenList = new ArrayList<Token>();
         for (char c : inputLine.toCharArray())
         {
             charArray.add(c);
@@ -23,15 +28,18 @@ public class Scanner
         for(int i=0; i<charArray.size(); i++) {
             String currentChar = charArray.get(i).toString();
             switch (currentState) {
-                case START:
 
-                    if (currentChar.matches("[a-zA-Z]")) {
+                case START:
+                    if (currentChar.matches("[a-zA-Z]"))
+                    {
+                        identifier1.concat(currentChar);
                         currentState = States.INID;
                     }
                     if (currentChar.matches(" ")) {
                         currentState = States.START;
                     }
                     if (currentChar.matches("[0-9]")) {
+                        previousState = States.START;
                         currentState = States.INNUM;
                     }
                     if (currentChar.matches("[{]")) {
@@ -39,6 +47,10 @@ public class Scanner
                     }
                     if (currentChar.matches("[:]")) {
                         currentState = States.INASSIGN;
+                    }
+                    if(currentChar.matches("[+|-|*|/|=|<|>|(|)|,|;]"))
+                    {
+                        currentState = States.DONE;
                     }
                     break;
 
@@ -51,39 +63,96 @@ public class Scanner
 
 
                 case INID:
-                    if (currentChar.matches("[a-zA-Z]")) {
+                    if (currentChar.matches("[a-zA-Z]"))
+                    {
+                        identifier1.concat(currentChar);
                         currentState = States.INID;
                     }
                     if (currentChar.matches(" ")) {
+                        identifierList.add(identifier1);
+                        identifier1 = new String();
                         currentState = States.START;
                     }
                     if (currentChar.matches("[0-9]")) {
+
+                        identifier1.concat(currentChar);
+                        previousState = States.INID;
                         currentState = States.INNUM;
                     }
                     if (currentChar.matches("[{]")) {
+
+                        identifierList.add(identifier1);
+                        identifier1 = new String();
                         currentState = States.INCOMMENT;
                     }
                     if (currentChar.matches("[:]")) {
+
+                        identifierList.add(identifier1);
+                        identifier1 = new String();
                         currentState = States.INASSIGN;
+                    }
+                    if(currentChar.matches("[+|-|*|/|=|<|>|(|)|,|;]"))
+                    {
+                        identifierList.add(identifier1);
+                        identifier1 = new String();
+                        currentState = States.DONE;
                     }
                     break;
 
 
                 case INNUM:
                     if (currentChar.matches("[a-zA-Z]")) {
+
+                        identifier1.concat(currentChar);
                         currentState = States.INID;
                     }
                     if (currentChar.matches(" ")) {
+
+                        if(previousState==States.INID)
+                        {
+                            identifierList.add(identifier1);
+                            identifier1 = new String();
+                        }
+
+                        if(previousState==States.START)
+                        {
+                            numberList.add(number1);
+                            number1 = new String();
+                        }
+
                         currentState = States.START;
                     }
                     if (currentChar.matches("[0-9]")) {
+
+                        number1.concat(currentChar).toString();
                         currentState = States.INNUM;
                     }
                     if (currentChar.matches("[{]")) {
+
+                        if(previousState==States.INID)
+                        {
+                            identifierList.add(identifier1);
+                            identifier1 = new String();
+                        }
                         currentState = States.INCOMMENT;
                     }
                     if (currentChar.matches("[:]")) {
+
+                        if(previousState==States.INID)
+                        {
+                            identifierList.add(identifier1);
+                            identifier1 = new String();
+                        }
                         currentState = States.INASSIGN;
+                    }
+                    if(currentChar.matches("[+|-|*|/|=|<|>|(|)|,|;]"))
+                    {
+                        if(previousState==States.INID)
+                        {
+                            identifierList.add(identifier1);
+                            identifier1 = new String();
+                        }
+                        currentState = States.DONE;
                     }
                     break;
 
--- src/Token.java
+++ src/Token.java
@@ -1,3 +1,10 @@
-public class Token {
-    public String[] tokenList = new String[100];
+public class Token
+{
+    public String tokenValue;
+    public String tokenType;
+    public Token(String tokenValue, String tokenType)
+    {
+        this.tokenValue = tokenValue;
+        this.tokenType = tokenType;
+    }
 }
